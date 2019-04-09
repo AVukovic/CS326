901fd0fdb957e9c9c0f8569cefea126757e64535
Alexander Vukovic <alexvukovic96@gmail.com>
2019-04-09 12:23:40 -0400

    begun settin up route handlers

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-app.use(bodyParser.json());
-
-
 const app = express();
 
 app.use(express.static('static'));
 app.use(bodyParser.json());
 
-app.listen(3000, function () {
+
+app.post('./App01', (request, response) => {
+    let msg = request.body;
+
+})
+
+let db;
+MongoClient.connect('mongodb://localhost', { useNewUrlParser: true }).then(connection => {
+  db = connection.db('EulerOnCanvas');
+  app.listen(3000, () => {
     console.log('App started on port 3000');
+  });
+}).catch(error => {
+  console.log('ERROR:', error);
 });
