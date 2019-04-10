3eb6dc3b51f989a842e7110751f8169d88598030
Alexander Vukovic <alexvukovic96@gmail.com>
2019-04-10 10:41:05 -0400

    making progress?

--- scripts/init.mongo.js
+++ scripts/init.mongo.js
@@ -2,15 +2,16 @@
 
 // Connect to the issuetracker database. Note, if the issuetracker database
 // does not exist, it will create it with this call.
-db = new Mongo().getDB('database');
+db = new Mongo().getDB('Euler');
+db.createCollection('messages');
 
 // Next, we remove everything inside it. This is helpful to ensure that the
 // database starts from a known state.
-db.messages.remove({});
+//db.messages.remove({});
 
 // Now, we insert some mock data that mirrors the data that we have in the
 // in-memory version of the `server.js` code.
-db.messages.insert([
+db.collection.insert([
   {
     _id: 'Ij Cpc',
     offset: 1
--- server.js
+++ server.js
@@ -8,19 +8,20 @@ app.use(bodyParser.json());
 
 
 app.post('/post', (request, response) => { const orgMsg = request.body; response.json(orgMsg); });
+
 app.get('/get', (req, res) => {
-  db.collection('messages').find().toArray().then((val, err) => {
-    res.json({ messages: val });
+  datab.collection('messages').find().toArray().then((val, err) => {
+    res.json({ entries: val });
   }).catch(error => {
     console.log(error);
     res.status(500).json({ message: `Internal Server Error: ${error}` });
   });
 });
 
-let db;
+let datab;
 const MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://localhost', { useNewUrlParser: true }).then(connection => {
-  db = connection.db('database');
+  datab = connection.db('Euler');
   app.listen(3000, () => {
     console.log('App started on port 3000');
   });
