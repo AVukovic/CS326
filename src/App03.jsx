3b3353953c772b41e15574f5b20aa8858a0b3384
Alexander Vukovic <alexvukovic96@gmail.com>
2019-04-30 13:54:37 -0400

    can't spell lmao

--- src/App03.jsx
+++ src/App03.jsx
@@ -49,7 +49,7 @@ class Body extends React.Component {
       <div className="row">
         <div className="col-2"></div><div className="col-2"></div>
         <div className="col-4 align-items-center" style={{ textAlign:'center' }}>
-          <p className="text-justify">Euler on Canvas is an app that allows you to encrpyt messages and decrpyt them at a later date. <br /><br /> 
+          <p className="text-justify">Euler on Canvas is an app that allows you to encrypt messages and decrypt them at a later date. <br /><br /> 
           We achieve this through a classic method of encryption- the <b>Caesarian Cypher</b>. The Caesarian Cypher works by offsetting each letter in a given string of text by a number between <b>1</b> and <b>25</b>. For example, if our offset value is <b>2</b>, the message <b>abc</b> would become <b>cde</b>. If our offset was <b>25</b>, the message would become <b>zab</b>.<br/><br />
           In order to decrypt messages, we use the coded messages and apply the inverse of the original cypher. The inverse is found by subtracting the offset from <b>26</b>. Harkening back to our original example, the inverted offset of <b>2</b> is now <b>24</b>. The string <b>cde</b> offset by <b>24</b> yields <b>abc</b>, our original message.</p>
         </div>
